9b5d1c41d8ed088c5cc29fb2d5e959fb19f813be
Jennifer Reiber Kyle <jreiberkyle@users.noreply.github.com>
2020-02-03 15:57:09 -0800

    change dims to inches

--- hexagonal_loom.js
+++ hexagonal_loom.js
@@ -1,6 +1,11 @@
-function drawHexagon(point_radius, center_point) {
+
+function drawHexagon(point_radius, thickness_px) {
     // Create a Paper.js Path to draw a line into it:
-    var hexagon = new Path({closed:true});
+    var hexagon = new Path({closed:true,
+                            strokeColor: 'black',
+                            strokeWidth: thickness_px,
+                            strokeJoin: 'round'
+    });
 
     // How many points do we want our object to have
     var points = 6;
@@ -28,20 +33,17 @@ function drawHexagon(point_radius, center_point) {
     return hexagon
 };
 
-var center_point = new Point(400, 400)
-var point_radius = 200;
-var hexagon = drawHexagon(point_radius, center_point); 
-hexagon.strokeColor = 'black';
-
-function circlesAtIntersections(hexagon, hole_count, hole_radius){
-    // draw vertical, evenly spaced lines
-    var spacing = hexagon.bounds.width/(hole_count-1);
-    if (spacing < 2.5 * hole_radius) {
-        console.error('spacing too small')
-    }
+function circlesAtIntersections(hexagon, spacing_in, hole_radius){
+    // // draw vertical, evenly spaced lines
+    // var spacing = hexagon.bounds.width/(hole_count-1);
+    // if (spacing < 2.5 * hole_radius) {
+    //     console.error('spacing too small')
+    // }
+    var spacing = spacing_in;
+    var hole_count = hexagon.bounds.width/spacing + 1;
     
     var lines = [];
-    for (var i=0; i < hole_count; i++) {
+    for (var i=1; i < hole_count - 1; i++) {
         
         var line = new Path.Line(hexagon.bounds.topLeft,
                                  hexagon.bounds.bottomLeft);
@@ -51,20 +53,45 @@ function circlesAtIntersections(hexagon, hole_count, hole_radius){
 
     var lines_path = new CompoundPath({
         children: lines,
-        strokeColor: 'blue',
+        strokeColor: 'red'
     });
     
     var intersectionGroup = new Group();
     var intersections = lines_path.getIntersections(hexagon);
-
+    
+    var circles = [];
     for (var i = 0; i < intersections.length; i++) {
         var intersectionPath = new Path.Circle({
             center: intersections[i].point,
             radius: hole_radius,
-            strokeColor: 'blue',
             parent: intersectionGroup
         });
+        circles.push(intersectionPath);
     }
+    var circles_path = new CompoundPath({
+        children: circles,
+        strokeColor: 'blue'
+    });
+    
+    return new Group([circles_path, lines_path]);
 }
 
-circlesAtIntersections(hexagon, 21, 5);
+// values are given in inches
+// need to scale by 96 upon display to save as inches
+var point_radius_in = 3,
+    thickness_in = .5,
+    spacing_in = .2,
+    // hole_count  = 21,
+    hole_radius_in = .05;
+var dpi = 96;
+
+var center_point = new Point(point_radius_in, point_radius_in);
+var hexagon = drawHexagon(point_radius_in, thickness_in*dpi);
+
+circlesGroup = circlesAtIntersections(hexagon, spacing_in, hole_radius_in);
+circles = circlesGroup.children[0];
+lines = circlesGroup.children[1];
+lines.remove();
+var hexagon_loom_group = new Group([hexagon, circles]);
+hexagon_loom_group.scale(dpi); // scale inches to pixels for display
+hexagon_loom_group.translate(center_point*dpi);
